a530ba7811ba25ac7674e9c2393b1bb21b051df3
DevUnusual <carlos.meneses.g.s@gmail.com>
2022-04-19 23:17:52 -0300

    rename baralho para cartas e iniciou da classe dealer

--- dealer.py
+++ dealer.py
@@ -1,2 +1,12 @@
-import baralho
+import cartas
 
+class Dealer():
+  def __init__(self):
+    self.mao = []
+    self.baralho = cartas.Baralho()
+
+  def darCarta(self):
+    return self.baralho.pushCarta()
+
+  def cartaDealer(self):
+    self.mao.append(self.baralho.pushCarta())
